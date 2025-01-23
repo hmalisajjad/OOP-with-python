a4d3ac52d94894124236766dca2c8e446e1bcc83
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2025-01-23 23:27:04 +0100

    defining constructor method attribute

--- SnakeTemp.py
+++ SnakeTemp.py
@@ -2,9 +2,17 @@ class Snake:
     """Snake main blueprint"""
     name = "Python"
 
+    # method to change name
+    def modifyName(self, new_name):
+        self.name = new_name
+
 #Objects based on Snake
 
 snake01 = Snake()
 
 # printing on screen
+print(snake01.name)
+
+# modify the name using constructor method modifyName
+snake01.modifyName("Anaconda")
 print(snake01.name)
