ccb3b1123dd8310a0041b41d4e2f23533d7d75d6
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2025-01-23 23:49:41 +0100

    just add more object instances

--- Person_blueprint.py
+++ Person_blueprint.py
@@ -6,8 +6,18 @@ class Person:
         self.personID = personID
 
 
-person01 = Person("Ali", 30, 12)
+person01 = Person("Ali", 30, 18)
+person02 = Person("Umer", 27, 16)
+person03 = Person("AbuBakr", 30, 19)
 
 print(person01.personID)
 print(person01.name)
 print(person01.age)
+
+print(person02.personID)
+print(person02.name)
+print(person02.age)
+
+print(person03.personID)
+print(person03.name)
+print(person03.age)
