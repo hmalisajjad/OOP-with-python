2108798eaf3135011a29e8ef5ef756ee1c19efdf
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2025-01-23 23:58:37 +0100

    using class methods

--- Person_blueprint.py
+++ Person_blueprint.py
@@ -5,19 +5,14 @@ class Person:
         self.age = age
         self.personID = personID
 
+    def display_data(self):
+        print("Hi my name is", self.name, ". I am", self.age, "years old and my ID is", self.personID)
+
 
 person01 = Person("Ali", 30, 18)
 person02 = Person("Umer", 27, 16)
-person03 = Person("AbuBakr", 30, 19)
+person03 = Person("AbuBakr", 25, 19)
 
-print(person01.personID)
-print(person01.name)
-print(person01.age)
-
-print(person02.personID)
-print(person02.name)
-print(person02.age)
-
-print(person03.personID)
-print(person03.name)
-print(person03.age)
+person01.display_data()
+person02.display_data()
+person03.display_data()
