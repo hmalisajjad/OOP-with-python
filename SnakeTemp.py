f961826b7dcd0563e6c352066c12ed7e2090b917
Hafiz Muhammad Ali Sajjad <alisajjad251992@gmail.com>
2025-01-23 23:19:04 +0100

    defining attribute

--- SnakeTemp.py
+++ SnakeTemp.py
@@ -1,10 +1,10 @@
 class Snake:
     """Snake main blueprint"""
-    pass
+    name = "Python"
 
 #Objects based on Snake
 
 snake01 = Snake()
 
 # printing on screen
-print(snake01)
+print(snake01.name)
